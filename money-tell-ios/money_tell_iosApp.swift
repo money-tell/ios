80f8d85a68de026d44948c435209279eaf6e228c
i.gerenko <i.gerenko@vk.team>
2022-07-26 22:43:17 +0300

    Initial Commit

---
+++ money-tell-ios/money_tell_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  money_tell_iosApp.swift
+//  money-tell-ios
+//
+//  Created by i.gerenko on 26.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct money_tell_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
